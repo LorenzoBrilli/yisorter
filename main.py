4d6b1a206c390237b65cbd5b102b10f6064bcf2d
Lorenzo Brilli <lorenzo.brilli.95@gmail.com>
2017-12-16 22:46:28 +0100

    sort modalitirs and create folders

--- main.py
+++ main.py
@@ -17,10 +17,20 @@ videos = [f for f in files if ('.mp4' in f.lower())]
 thms = [f for f in files if ('.thm' in f.lower())]
 secs = [f for f in files if ('.secs' in f.lower())]
 
-#sort files
+#create photo and video directories
+if (len(photos)>0):
+    mkdir('photos')
+if (len(videos)>0):
+    mkdir('videos')
+
+#sort modalities
 p_photos = [f for f in photos if (f.lower()[:4]=='ydxj')]
 p_bursts = [f for f in photos if (f.lower()[:1]=='c')]
 p_timelapses = [f for f in photos if (f.lower()[:1]=='y' and f.lower()[1:2]!='d')]
+p_photofromvideos = [f for f in photos if (f.lower()[:1]=='t')]
+v_videos = [f for f in videos if ((f.lower()[:4]=='ydxj') or (f.lower()[:2]=='yn') or (f.lower()[:2]=='yp'))]
+v_timelapses = [f for f in videos if ((f.lower()[:4]=='ydtl') or (f.lower()[:2]=='yt'))]
+v_slowmotion = [f for f in videos if ((f.lower()[:2]=='ys'))]
 
 
 #remove all thms and secs
