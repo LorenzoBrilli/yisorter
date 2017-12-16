603646a74f6d19b194997c6f7c3e552a6d522b38
Lorenzo Brilli <lorenzo.brilli.95@gmail.com>
2017-12-16 23:20:50 +0100

    final version v 1

--- main.py
+++ main.py
@@ -22,8 +22,10 @@ p_photos = [f for f in photos if (f.lower()[:4]=='ydxj')]
 p_bursts = [f for f in photos if (f.lower()[:1]=='c')]
 p_timelapses = [f for f in photos if (f.lower()[:1]=='y' and f.lower()[1:2]!='d')]
 p_photofromvideos = [f for f in photos if (f.lower()[:1]=='t')]
-v_videos = [f for f in videos if ((f.lower()[:4]=='ydxj') or (f.lower()[:2]=='yn') or (f.lower()[:2]=='yp'))]
+v_videos = [f for f in videos if ((f.lower()[:4]=='ydxj') or (f.lower()[:2]=='yn'))]
 v_timelapses = [f for f in videos if ((f.lower()[:4]=='ydtl') or (f.lower()[:2]=='yt'))]
+v_loop = [f for f in videos if (f.lower()[:1]=='l')]
+v_videoandphotos = [f for f in videos if (f.lower()[:2]=='yp')]
 v_slowmotion = [f for f in videos if ((f.lower()[:2]=='ys'))]
 
 #create photo and video directories
@@ -38,8 +40,34 @@ if (len(videos)>0):
     except:
         pass
 
-#sort
-rename('a.jpg','YDXJ0001.JPG')
+#sort files
+for f in p_photos:
+    rename(f,('photos/photo_'+f[4:]).lower())
+for f in p_bursts:
+    rename(f,('photos/burst_'+f[1:4]+'_'f[4:]).lower())
+for f in p_timelapses:
+    rename(f,('photos/timelapse_'+f[1:4]+'_'f[4:]).lower())
+for f in p_photofromvideos:
+    rename(f,('photos/photofromvideo_'+f[1:4]+'_'f[4:]).lower())
+for f in v_videos:
+    if (f[2:4].lower() == 'xj'):
+        f = f[:2] + '00' + f[4:]
+    rename(f,('videos/video_'+f[4:8]+'_'+f[2:4]+f[8:]).lower())
+for f in v_timelapses:
+    if (f[2:4].lower() == 'tl'):
+        f = f[:2] + '00' + f[4:]
+    rename(f,('videos/timelapse_'+f[4:8]+'_'+f[2:4]+f[8:]).lower())
+for f in v_loop:
+    rename(f,('videos/loop_'+f[4:8]+'_'+f[1:4]+f[8:]).lower())
+for f in v_videoandphotos:
+    if (f[2:4].lower() == 'xj'):
+        f = f[:2] + '00' + f[4:]
+    rename(f,('videos/videowphotos_'+f[4:8]+'_'+f[2:4]+f[8:]).lower())
+for f in v_slowmotion:
+    if (f[2:4].lower() == 'xj'):
+        f = f[:2] + '00' + f[4:]
+    rename(f,('videos/slowmotion_'+f[4:8]+'_'+f[2:4]+f[8:]).lower())
+
 
 #remove all thms and secs
 for f in thms:
